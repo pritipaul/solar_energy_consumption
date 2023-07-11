7c1bfb9d16c891dc1f5c2950a9d2a15f426bcae9
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2023-07-11 19:42:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def run():
     Time23:00 = st.number_input("11:00 pm", min_value=0)
     Time23:30 = st.number_input("11:30 pm", min_value=0)
     Time0:00 = st.number_input("12:00 am", min_value=0)
-    Energy Consumption = st.number_input("Energy Consumption", min_value=0)
+    Energy_Consumption = st.number_input("Energy Consumption", min_value=0)
     CL	= st.radio("CL", [0, 1])
     GC	= st.radio("GC", [0, 1])
     GG	= st.radio("GG", [0, 1])
@@ -84,7 +84,7 @@ def run():
     date_day = st.number_input("Date_Day", min_value=00)
 
     if st.button("Predict"):
-        features = [Time0:30,Time1:00,Time1:30,Time2:00,Time2:30,Time3:00,Time3:30,Time4:00,Time4:30,Time5:00,Time5:30,Time6:00,Time6:30,Time7:00,Time7:30,Time8:00,Time8:30,Time9:00,Time9:30,Time10:00,Time10:30,Time11:00,Time11:30,Time12:00,Time12:30,Time13:00,Time13:30,Time14:00,Time14:30,Time15:00,Time15:30,Time16:00,Time16:30,Time17:00,Time17:30,Time18:00,Time18:30,Time19:00,Time19:30,Time20:00,Time20:30,Time21:00,Time21:30,Time22:00,Time22:30,Time23:00,Time23:30,Time0:00,Energy Consumption,CL,GC,GG,date_year,date_month,date_day]
+        features = [Time0:30,Time1:00,Time1:30,Time2:00,Time2:30,Time3:00,Time3:30,Time4:00,Time4:30,Time5:00,Time5:30,Time6:00,Time6:30,Time7:00,Time7:30,Time8:00,Time8:30,Time9:00,Time9:30,Time10:00,Time10:30,Time11:00,Time11:30,Time12:00,Time12:30,Time13:00,Time13:30,Time14:00,Time14:30,Time15:00,Time15:30,Time16:00,Time16:30,Time17:00,Time17:30,Time18:00,Time18:30,Time19:00,Time19:30,Time20:00,Time20:30,Time21:00,Time21:30,Time22:00,Time22:30,Time23:00,Time23:30,Time0:00,Energy_Consumption,CL,GC,GG,date_year,date_month,date_day]
         predicted_label = predict_energy_consumption(features)
         st.write("Predicted Label:", predicted_label)
   
